de1c51a3d8bbb705e73bb4c9ac7d955792b3e279
Arsh369 <playgames1797@gmail.com>
2026-01-15 09:35:31 +0530

    /blog

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
         <Link to={'/'} className='text-gray-400 font-semibold bg-gray-900 px-3 py-1 rounded-2xl'>Home</Link>
         <Link to={'/about'} className='text-gray-400 font-semibold bg-gray-900 px-3 py-1 rounded-2xl'>About</Link>
         <Link  className='text-gray-400 font-semibold bg-gray-900 px-3 py-1 rounded-2xl'>Projects</Link>
-        <Link  className='text-gray-400 font-semibold bg-gray-900 px-3 py-1 rounded-2xl'>Blog</Link>
+        <Link to={'/blog'} className='text-gray-400 font-semibold bg-gray-900 px-3 py-1 rounded-2xl'>Blog</Link>
         <Link  className='text-gray-400 font-semibold bg-gray-900 px-3 py-1 rounded-2xl'>Contact</Link>
     </nav>
   )
